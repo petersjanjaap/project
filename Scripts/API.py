aec2fcba16762878c7671c2d9efb2c5cac875a93
petersjanjaap <43883509+petersjanjaap@users.noreply.github.com>
2019-01-10 12:39:22 +0100

    scripts

---
+++ Scripts/API.py
@@ -0,0 +1,201 @@
+# import libraries
+import requests
+import json
+from collections import defaultdict
+
+# define globals
+DATA = defaultdict(int)
+COUNTRIES = defaultdict(int)
+
+
+# define functions
+def init_countries(object):
+    """
+    obtains info on countries and returns as dict
+    """
+    countries = defaultdict(int)
+
+    # initialize data based on countries in dataset
+    for i, h in enumerate(object['structure']['dimensions']['series'][2]
+                          ['values']):
+
+        # generate new dictionary behind each country
+        countries[i] = [h['id'], h['name']]
+
+        # check if country is already added to global countries
+        if h['id'] not in COUNTRIES:
+            COUNTRIES[h['id']] = h['name']
+
+    # return the dictionary
+    return countries
+
+
+def init_sectors(object):
+    """
+    get codes for sectors
+    """
+
+    # initialize dictionary to hold sectors
+    sectors = defaultdict(int)
+
+    # codes for sectors are stored in json file
+    codes = object['structure']['dimensions']['series'][3]['values']
+    for i, code in enumerate(codes):
+
+        # use code as key and id and name as values in list
+        sectors[i] = [code['id'], code['name']]
+
+    # return the dictionary
+    return sectors
+
+
+def country_dict(year, country_id):
+
+        # if not, add entry for this country
+        DATA[year][country_id] = defaultdict(str)
+
+        # add dictionary for two datasets
+        DATA[year][country_id]['share'] = defaultdict(str)
+        DATA[year][country_id]['trade'] = defaultdict(str)
+
+        # put extra dictionaries for sectors in trade dataset
+        DATA[year][country_id]['trade']['xprt'] = defaultdict(str)
+        DATA[year][country_id]['trade']['mprt'] = defaultdict(str)
+
+
+def load_data(link, year, type):
+    """
+    this function loads DATA on trade partner percentages in total trade or
+    DATA on total trade given a link to world banks API
+    """
+
+    # load data containing info on trade value in $
+    response = requests.get(link)
+
+    # read as JSON object
+    j = json.loads(response.content)
+
+    # obtain code on countries
+    countries = init_countries(j)
+
+    """
+    add new dictionaries for both this year for all countries if the link type
+    is shares, because this set is read through for each year and they both
+    contain the same countries
+    """
+    if type == 'share':
+
+        # add countries to dictionary for this year
+        for country in countries:
+
+            # use country id as key
+            cid = countries[country][0]
+            country_dict(year, cid)
+
+    # sectors are only available for trade dataset
+    if type == 'trade':
+
+        # obtain code on countries
+        sectors = init_sectors(j)
+
+    # iterate over country keys in dataset
+    for key in j['dataSets'][0]['series']:
+
+        """
+        the key n:n:n:n:n contains at the middle position the country code, at
+        the second last the sector code and in the last position the imprt
+        indicator
+        """
+        codes = key.split(':')
+        country = int(codes[2])
+        sector = int(codes[3])
+        mprt = int(codes[4])
+
+        # get sector and country string codes
+        country = countries[country][0]
+
+        # double check if country is present in both dataset
+        if country not in DATA[year]:
+
+            # if not add entries to dictionary
+            country_dict(year, country)
+
+            # add missing entries
+            DATA[year][country]['share']['mprt'] = None
+            DATA[year][country]['share']['xprt'] = None
+
+        # sectors are only available for trade dataset
+        if type == 'trade':
+            sector = sectors[sector][0]
+
+        """
+        the observation value is in the first element of the dictionary in the
+        value
+        """
+        obs = j['dataSets'][0]['series'][key]['observations']['0'][0]
+
+        # check where data should be placed
+        if mprt:
+
+            if type == 'share':
+                DATA[year][country][type]['mprt'] = obs
+            else:
+
+                # put info per sector
+                DATA[year][country][type]['mprt'][sector] = obs
+        else:
+
+            # put info for export
+            if type == 'share':
+                DATA[year][country]['share']['xprt'] = obs
+
+            else:
+                # put info per sector
+                DATA[year][country][type]['xprt'][sector] = obs
+
+
+def json_save(dictionary, save_title):
+    """
+    saves dictionary as json file
+    """
+    j_data = json.dumps(dictionary)
+    f = open(save_title, "w")
+    f.write(j_data)
+
+
+def main():
+    """
+    loads all data from API into python dictionary, which is converted to JSON
+    """
+
+    # iterate over years 2000-2017
+    for year in range(2000, 2017):
+
+        #  add position to hold information for year
+        DATA[year] = defaultdict(str)
+
+        """
+        create links for API, world bank does not allow to download all years
+        at once
+        """
+
+        # adjust API for given year
+        link_shares = f'http://wits.worldbank.org/API/V1/SDMX/V21/datasource/tradestats-trade/reporter/usa/year/{year}/partner/ /product/ /indicator/XPRT-PRTNR-SHR;MPRT-PRTNR-SHR?format=JSON'
+        link_values = f'http://wits.worldbank.org/API/V1/SDMX/V21/datasource/tradestats-trade/reporter/gbr/year/{year}/partner/ALL/product/Total;AgrRaw;Chemical;Food;Fuels;manuf;OresMtls;Textiles;Transp/indicator/XPRT-TRD-VL;MPRT-TRD-VL?format=JSON'
+
+        # load data for partner shares
+        load_data(link_shares, year, 'share')
+
+        # load data for trade values
+        load_data(link_values, year, 'trade')
+
+    # convert dataset and countries info to JSON objects and save these
+    json_save(DATA, 'dataset.json')
+    json_save(COUNTRIES, 'countries.json')
+
+
+if __name__ == '__main__':
+    """
+    execute functions
+    """
+    main()
